49bd6bda286cce67694b2a333c61bb3bbc09c0b4
putiwangsheng <2026929156@qq.com>
2015-07-06 16:24:29 +0800

    add ex10-13

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print("How old are you,fish tail", end=" ")
+age = input()
+print("How tall are you?", end=" ")
+height = input()
+print("How much do you weigh", end=" ")
+weight = input()
+
+print("So,you're %s old,%s tall,%s heavy" % (age, height, weight))
---
+++ ex12.py
@@ -0,0 +1,4 @@
+age = input("How old are you?")
+height = input("How tall are you?")
+
+print("So,you are %s old,%s tall" % (age, height))
