11417adfb0c5d10b27a19e0495be072efd21446d
putiwangsheng <2026929156@qq.com>
2015-07-12 17:10:45 +0800

    exercise

--- ex20.py
+++ ex20.py
@@ -5,7 +5,8 @@ script, filename = argv
 
 def print_file(f):
     print(f.readlines())  # 返回一个列表，列表中是文件内容
-    print(f.readline())  # 返回一行字符串
+    print(f.readline())  # 返回一行字符串,readline()里面的代码会扫面文件的每一个字节，
+                         # 直到找到一个\n,停止读取文件，并返回之前读取到的文件内容。
 
 
 def read_file(f):
