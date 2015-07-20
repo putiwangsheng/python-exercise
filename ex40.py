cb639faa6faa3ddc215738c89b50a470a1d2791d
putiwangsheng <2026929156@qq.com>
2015-07-20 13:55:03 +0800

    add __str__(self)

--- ex40.py
+++ ex40.py
@@ -3,6 +3,9 @@ class Song(object):
     def __init__(self, lyrics):
         self.lyrics = lyrics
 
+    def __str__(self):
+        return str(self.lyrics)
+
     def sing_me_a_song(self):
         for line in self.lyrics:
             print(line)
@@ -10,8 +13,10 @@ class Song(object):
 # 创建对象，__init__函数被调用来初始化对象
 birthday = ["Happy brithday to you", "I don't want to get sued"]
 happy_bday = Song(birthday)
+print(Song(birthday))
 parade = ["They rally round the family"]
 bulls_on_parade = Song(parade)
+print(Song(Song(parade)))
 
 happy_bday.sing_me_a_song()
 bulls_on_parade.sing_me_a_song()
