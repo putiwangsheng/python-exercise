9522d3d9cde52c34ac6fcc2a48f1f004d1f08e23
putiwangsheng <2026929156@qq.com>
2015-07-22 11:12:43 +0800

    exist an error

--- ex41.py
+++ ex41.py
@@ -1,5 +1,5 @@
 import random
-from urllib import urlopen
+from urllib.request import urlopen
 import sys
 
 WORD_URL = "http://learncodethehardway.org/words.txt"
@@ -25,14 +25,14 @@ if len(sys.argv) == 2 and sys.argv == "english":
 
 for word in urlopen(WORD_URL).readlines():
     # 函数strip()作用为去除字符串首尾空格（没有参数的时候）
-    WORDS.append(word.strip())
+    WORDS.append(word.strip().decode('utf-8'))
 
 def convert(snippet, phrase):
     # sample(list，k)函数表示从列表中随机获取k个元素
     # capitalize()实现字符串首字母大写，其余小写
     # count()，统计某个元素在列表中出现的次数
     class_names = [w.capitalize() for w in
-                    random.sample(WORDS, snippet.count("%%%"))]
+                    random.sample(WORDS, snippet.count("%%%"))]  # 列表生成式
     other_names = random.sample(WORDS, snippet.count("***"))
     results = []
     param_names = []
@@ -40,7 +40,7 @@ def convert(snippet, phrase):
     for i in range(0, snippet.count("@@@")):
         # random.randint(1, 3)指随机生成1-3范围内的数（1<=x<=3）
         param_count = random.randint(1, 3)
-        param_names.append(', '.join(random.sample(WORDS, param_count)))
+        param_names.append(','.join(random.sample(WORDS, param_count)))
 
     for sentence in snippet, phrase:
         # sentence[:]复制列表
@@ -59,16 +59,16 @@ def convert(snippet, phrase):
     results.append(result)
     return results
 
+
 try:
     while True:
-        # keys()，以列表返回字典中所有的键
-        snippet = PHRASES.keys()
-        # random.shuffle()函数用于将列表中的元素随机打乱
-        random.shuffle(snippet)
+        snippets = list(PHRASES.keys())
+        random.shuffle(snippets)
 
         for snippet in snippets:
             phrase = PHRASES[snippet]
             question, answer = convert(snippet, phrase)
+            answer = convert(snippet, phrase)
             if PHRASE_FIRST:
                 question, answer = answer, question
 
